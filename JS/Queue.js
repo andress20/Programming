5e5b9a05d9e632e792f4799c24d630a9f359c93f
andress20 <andress20@gmail.com>
2020-10-12 19:53:10 -0500

    Queue implementation

--- JS/Queue.js
+++ JS/Queue.js
@@ -1,27 +1,24 @@
 function Queue() {
+	this.dataStore = Array.prototype.slice.call(arguments, 0)
+	this.enqueue = enqueue
+	this.dequeue = dequeue
+	this.emptyQueue = emptyQueue
+	this.isEmpty = isEmpty
+	this.print = print
 
-		this.dataStore = Array.prototype.slice.call(arguments, 0);
-		this.enqueue = enqueue;
-		this.dequeue = dequeue;
-		this.emptyQueue = emptyQueue;
-		this.isEmpty = isEmpty
-		this.print = print;
-
-		function enqueue(element) {
-				this.dataStore.push(element);
-		}
-
-		function dequeue() {
-				return this.dataStore.shift();
-		}
-
-		function emptyQueue() {
-				return this.dataStore = [];
-		}
-		function isEmpty(){
-			return this.dataStore.length ? false : true
-		}
-		function print(){
-			console.log(this.dataStore)
-		}
+	function enqueue(element) {
+		this.dataStore.push(element);
+	}
+	function dequeue() {
+		return this.dataStore.shift();
+	}
+	function emptyQueue() {
+		return this.dataStore = [];
+	}
+	function isEmpty(){
+		return this.dataStore.length ? false : true
+	}
+	function print(){
+		console.log(this.dataStore)
+	}
 }
